4bd96a854fed69351686cc12d4c32e8879f2d4a7
Vishalpcd <vpathak154@gmail.com>
2017-06-05 22:11:49 +0530

    C# projects for printing right angle triangle.
    To print right angle triangle as below
    *
    **
    ***
    ****
    *****

---
+++ Pattern_programs/Pattern_programs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pattern_programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how many rows you want ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= r; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
